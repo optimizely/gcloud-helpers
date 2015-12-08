e6db22af6f91b24c555fc0d49eccf568819cbe8f
Jon San Miguel <sanmiguelje@yahoo.com>
2015-12-07 16:22:40 -0800

    Add Manifest and bump patch version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name='gcloud_helpers',
-  version='0.1.0',
+  version='0.1.1',
   author='Yonatan Kogan',
   author_email='yonatan@optimizely.com',
   packages=['gcloud_helpers'],
