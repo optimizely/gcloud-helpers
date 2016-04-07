1ba0068baaf69883d0f249d7cfa8c68b6eb05d0c
Eric Higgins <erichiggins@gmail.com>
2016-04-07 13:18:28 -0700

    Upgrade to oauth2client 2.0.1

--- gcloud_helpers/connection_helper.py
+++ gcloud_helpers/connection_helper.py
@@ -2,13 +2,13 @@ from gcloud import datastore
 from gcloud.datastore import SCOPE
 from gcloud.datastore.connection import Connection
 
-from oauth2client import client
+from oauth2client import service_account
 
 def get_connection(client_email, private_key_string):
-  svc_account_credentials = client.SignedJwtAssertionCredentials(
-    service_account_name=client_email,
-    private_key=private_key_string,
-    scope=SCOPE)
+  svc_account_credentials = service_account.ServiceAccountCredentials._from_p12_keyfile_contents(
+    service_account_email=client_email,
+    private_key_pkcs12=private_key_string,
+    scopes=SCOPE)
 
   return Connection(credentials=svc_account_credentials)
 
